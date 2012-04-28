22b398ac83404a23bf9c71196fbca58a72bead49
betobates <rbwestmoreland@gmail.com>
2012-04-28 00:56:39 -0400

    Updated build.cmd to only build the Jabbot.Console project

--- build.cmd
+++ build.cmd
@@ -7,5 +7,5 @@ if "%config%" == "" (
 md artifacts
 md artifacts\bin
 md artifacts\packages
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild Jabbot.sln /p:Configuration="%config%";OutputPath=..\artifacts\bin\;BuildPackage=false;PackageOutputDir=..\artifacts\packages /m /v:M /fl /flp:LogFile=artifacts\msbuild.log;Verbosity=Normal /nr:false
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild Jabbot.Console\Jabbot.Console.csproj /p:Configuration="%config%";OutputPath=..\artifacts\bin\;BuildPackage=false;PackageOutputDir=..\artifacts\packages /m /v:M /fl /flp:LogFile=artifacts\msbuild.log;Verbosity=Normal /nr:false
 ::pause
